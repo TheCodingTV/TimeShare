f382f5d12b7ad7e63fcd4de61d2926a16911c6c8
TheCodingTV <496356079@qq.com>
2022-04-01 20:25:51 +0800

    update

--- src/Pages-Mini/HomePage/index.js
+++ src/Pages-Mini/HomePage/index.js
@@ -3,13 +3,8 @@ import Timer from '../../Controllers/Timer'
 
 export default function HomePage () {
   return (
-    <Timer
-      config={[
-        { totalSeconds: 210, componentType: 'card' },
-      ]}
-      onMoveNext={() => {}}
-      sectionId={'abc'}
-      hasNextStep={false}
-    />
+    <div>
+      hello TimeShare
+    </div>
   )
 }
